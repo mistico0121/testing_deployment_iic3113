9abb83019833f2fd77a48b4034c97ff3b4667529
mistico0121 <mrschudeck@uc.cl>
2020-08-29 15:27:24 -0400

    mediafiles

--- app/forum/settings.py
+++ app/forum/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
