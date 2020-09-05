474521e4db6e74e7c28ed06ab62185e64fad8353
Ubuntu <ubuntu@ip-172-31-51-119.ec2.internal>
2020-09-05 06:47:26 +0000

    arreglar esto

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(testimageboard.tk www.testimageboard.tk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mrschudeck@uc.cl" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -64,7 +64,7 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "1" ]; then staging_arg="--staging"; fi
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
