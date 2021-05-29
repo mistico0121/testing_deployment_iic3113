0cee9210ba50576585d0020cce58045e1487ffe4
mistico0121 <mrschudeck@uc.cl>
2021-05-29 17:41:31 -0400

    for development

--- app/forum/settings.py
+++ app/forum/settings.py
@@ -39,7 +39,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ['testimageboard.tk','www.testimageboard.tk', '34.207.190.96']
+ALLOWED_HOSTS = ['testimageboard.tk','www.testimageboard.tk', '34.207.190.96', 'localhost']
 
 
 # Application definition
@@ -145,5 +145,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = False
