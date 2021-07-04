30cc08e3ff2e69d9fdfb71c3c474c6ea67c914e2
mistico0121 <mrschudeck@uc.cl>
2021-07-04 18:18:30 -0400

    lets fucking goooo

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(testimageboard.tk www.testimageboard.tk)
+domains=(swdev9.ing.puc.cl www.swdev9.ing.puc.cl)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mrschudeck@uc.cl" # Adding a valid address is strongly recommended
